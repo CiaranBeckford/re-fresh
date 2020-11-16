7b9ab2011cfbb5b91ec242b2b616549ae9150750
Ciaran Beckford <ciaranbeckford@gmail.com>
2020-11-16 13:25:01 -0500

    Base Project

---
+++ refresh/refresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  refresh
+//
+//  Created by Ciaran Beckford on 11/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
