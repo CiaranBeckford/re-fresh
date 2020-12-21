8bec93a75a9996fe1770be4e062889e0a964c4fc
alice-zhang <ayz2105@columbia.edu>
2020-12-20 17:00:27 -0800

    Updates shopping cart view

--- refresh/Home/NavigationLanding/ShoppingCart.swift
+++ refresh/Home/NavigationLanding/ShoppingCart.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct ShoppingCart: View {
-    @ObservedObject var fridgeItems = FridgeItems()
+    @ObservedObject var cartItems = CartItems()
+    
     var body: some View {
-         Text("Shopping Cart")
         
+        VStack{
+            NavigationView {
+                List(cartItems.items) { cartItem in
+                  HStack{
+                      Image(cartItem.pic)
+                      VStack{
+                          HStack{
+                              Text(cartItem.action)
+                              Spacer()
+                          }
+                      }
+
+                      Spacer()
+                  }
+                  .padding()
+                  .frame(width: UIScreen.main.bounds.width*0.85, height: 60)
+                  .background(Color.boneWhite)
+                  .cornerRadius(10)
+                  .shadow(color: .gray, radius: 5, x: 5, y: 5)
+               }.navigationBarTitle(Text("Shopping Cart"))
+            }
+            Button(action: {
+                // Example of how to append item: self.fridgeItems.items.append(Item(action: "Potato", pic: "potato", expiration: 60.0, account: "profile2"))
+            }) {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .frame(width: 65, height:65)
+                    .foregroundColor(Color.teal)
+                    .padding()
+            }
+            
+            
+        }
     }
 }
 
